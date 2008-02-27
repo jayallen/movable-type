b858391e1abd571647b381b5e82080914d10968a
mpaschal <mpaschal@f3b680aa-fef1-0310-ac4d-b15a123b9462>
2008-02-27 16:26:30 +0000

    Try moving files around

---
+++ plugins/AtomServer/lib/MT/App/Atompub.pm
@@ -0,0 +1,395 @@
+# Movable Type (r) Open Source (C) 2001-2008 Six Apart, Ltd.
+# This program is distributed under the terms of the
+# GNU General Public License, version 2.
+#
+# $Id: AtomServer.pm 1174 2008-01-08 21:02:50Z bchoate $
+
+package MT::AtomServer;
+use strict;
+
+use MT::I18N qw( encode_text );
+use XML::Atom;
+use XML::Atom::Util qw( first textValue );
+use base qw( MT::App );
+use MIME::Base64 ();
+use Digest::SHA1 ();
+use MT::Atom;
+use MT::Util qw( encode_xml );
+use MT::Author;
+
+use constant NS_SOAP => 'http://schemas.xmlsoap.org/soap/envelope/';
+use constant NS_WSSE => 'http://schemas.xmlsoap.org/ws/2002/07/secext';
+use constant NS_WSU => 'http://schemas.xmlsoap.org/ws/2002/07/utility';
+
+sub init {
+    my $app = shift;
+    $app->{no_read_body} = 1
+        if $app->request_method eq 'POST' || $app->request_method eq 'PUT';
+    $app->SUPER::init(@_) or return $app->error("Initialization failed");
+    $app->request_content
+        if $app->request_method eq 'POST' || $app->request_method eq 'PUT';
+    $app->add_methods(
+        handle => \&handle,
+    );
+    $app->{default_mode} = 'handle';
+    $app->{is_admin} = 0;
+    $app->{warning_trace} = 0;
+    $app;
+}
+
+sub handle {
+    my $app = shift;
+
+    my $out = eval {
+        (my $pi = $app->path_info) =~ s!^/!!;
+        my($subapp, @args) = split /\//, $pi;
+        $app->{param} = {};
+        for my $arg (@args) {
+            my($k, $v) = split /=/, $arg, 2;
+            $app->{param}{$k} = $v;
+        }
+        if (my $action = $app->get_header('SOAPAction')) {
+            $app->{is_soap} = 1;
+            $action =~ s/"//g; # "
+            my($method) = $action =~ m!/([^/]+)$!;
+            $app->request_method($method);
+        }
+        my $apps = $app->config->AtomApp;
+        if (my $class = $apps->{$subapp}) {
+            bless $app, $class;
+        }
+        my $out = $app->handle_request;
+        return unless defined $out;
+        if ($app->{is_soap}) {
+            $out =~ s!^(<\?xml.*?\?>)!!;
+            $out = <<SOAP;
+$1
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+    <soap:Body>$out</soap:Body>
+</soap:Envelope>
+SOAP
+        }
+        return $out;
+    };
+    if (my $e = $@) {
+        $app->error(500, $e);
+        $app->show_error("Internal Error");
+    }
+    return $out;
+}
+
+sub handle_request {
+    1;
+}
+
+sub error {
+    my $app = shift;
+    my($code, $msg) = @_;
+    return unless ref($app);
+    if ($code && $msg) {
+        $app->response_code($code);
+        $app->response_message($msg);
+    }
+    elsif ($code) {
+        return $app->SUPER::error($code);
+    }
+    return undef;
+}
+
+sub show_error {
+    my $app = shift;
+    my($err) = @_;
+    chomp($err = encode_xml($err));
+    if ($app->{is_soap}) {
+        my $code = $app->response_code;
+        if ($code >= 400) {
+            $app->response_code(500);
+            $app->response_message($err);
+        }
+        return <<FAULT;
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <soap:Fault>
+      <faultcode>$code</faultcode>
+      <faultstring>$err</faultstring>
+    </soap:Fault>
+  </soap:Body>
+</soap:Envelope>
+FAULT
+    } else {
+        $app->response_content_type('text/xml');
+        return <<ERR;
+<error>$err</error>
+ERR
+    }
+}
+
+sub get_auth_info {
+    my $app = shift;
+    my %param;
+    if ($app->{is_soap}) {
+        my $xml = $app->xml_body;
+        my $auth = first($xml, NS_WSSE, 'UsernameToken');
+        $param{Username} = textValue($auth, NS_WSSE, 'Username');
+        $param{PasswordDigest} = textValue($auth, NS_WSSE, 'Password');
+        $param{Nonce} = textValue($auth, NS_WSSE, 'Nonce');
+        $param{Created} = textValue($auth, NS_WSU, 'Created');
+    } else {
+        my $req = $app->get_header('X-WSSE') or return;
+        $req =~ s/^WSSE //;
+        my ($profile);
+        ($profile, $req) = $req =~ /(\S+),?\s+(.*)/;
+        return $app->error(400, "Unsupported WSSE authentication profile") 
+            if $profile !~ /\bUsernameToken\b/i;
+        for my $i (split /,\s*/, $req) {
+            my($k, $v) = split /=/, $i, 2;
+            $v =~ s/^"//;
+            $v =~ s/"$//;
+            $param{$k} = $v;
+        }
+    }
+    \%param;
+}
+
+sub authenticate {
+    my $app = shift;
+    my $auth = $app->get_auth_info
+        or return $app->auth_failure(401, "Unauthorized");
+    for my $f (qw( Username PasswordDigest Nonce Created )) {
+        return $app->auth_failure(400, "X-WSSE requires $f")
+            unless $auth->{$f};
+    }
+    require MT::Session;
+    my $nonce_record = MT::Session->load($auth->{Nonce});
+    
+    if ($nonce_record && $nonce_record->id eq $auth->{Nonce}) {
+        return $app->auth_failure(403, "Nonce already used");
+    }
+    $nonce_record = new MT::Session();
+    $nonce_record->set_values({
+        id => $auth->{Nonce},
+        start => time,
+        kind => 'AN'
+    });
+    $nonce_record->save();
+# xxx Expire sessions on shorter timeout?
+    my $enc = $app->config('PublishCharset');
+    my $username = encode_text($auth->{Username},undef,$enc);
+    my $user = MT::Author->load({ name => $username, type => 1 })
+        or return $app->auth_failure(403, 'Invalid login');
+    return $app->auth_failure(403, 'Invalid login')
+        unless $user->api_password;
+    return $app->auth_failure(403, 'Invalid login')
+        unless $user->is_active;
+    my $created_on_epoch = $app->iso2epoch($auth->{Created});
+    if (abs(time - $created_on_epoch) > $app->config('WSSETimeout')) {
+        return $app->auth_failure(403, 'X-WSSE UsernameToken timed out');
+    }
+    $auth->{Nonce} = MIME::Base64::decode_base64($auth->{Nonce});
+    my $expected = Digest::SHA1::sha1_base64(
+         $auth->{Nonce} . $auth->{Created} . $user->api_password);
+    # Some base64 implementors do it wrong and don't put the =
+    # padding on the end. This should protect us against that without
+    # creating any holes.
+    $expected =~ s/=*$//;
+    $auth->{PasswordDigest} =~ s/=*$//;
+    #print STDERR "expected $expected and got " . $auth->{PasswordDigest} . "\n";
+    return $app->auth_failure(403, 'X-WSSE PasswordDigest is incorrect')
+        unless $expected eq $auth->{PasswordDigest};
+    $app->{user} = $user;
+
+    ## update session so the user will be counted as active
+    require MT::Session;
+    my $sess_active = MT::Session->load( { kind => 'UA', name => $user->id } );
+    if (!$sess_active) {
+        $sess_active = MT::Session->new;
+        $sess_active->id($app->make_magic_token());
+        $sess_active->kind('UA'); # UA == User Activation
+        $sess_active->name($user->id);
+    }
+    $sess_active->start(time);
+    $sess_active->save;
+    return 1;
+}
+
+sub auth_failure {
+    my $app = shift;
+    $app->set_header('WWW-Authenticate', 'WSSE profile="UsernameToken"');
+    $app->error(shift, 'Unauthorized');
+    return $app->show_error(@_);
+}
+
+sub xml_body {
+    my $app = shift;
+    unless (exists $app->{xml_body}) {
+        if (LIBXML) {
+            my $parser = XML::LibXML->new;
+            $app->{xml_body} = $parser->parse_string($app->request_content);
+        } else {
+            my $xp = XML::XPath->new(xml => $app->request_content);
+            $app->{xml_body} = ($xp->find('/')->get_nodelist)[0];
+        }
+    }
+    $app->{xml_body};
+}
+
+sub atom_body {
+    my $app = shift;
+    my $atom;
+    if ($app->{is_soap}) {
+        my $xml = $app->xml_body;
+        $atom = MT::Atom::Entry->new(Elem => first($xml, NS_SOAP, 'Body'))
+            or return $app->error(500, MT::Atom::Entry->errstr);
+    } else {
+        $atom = MT::Atom::Entry->new(Stream => \$app->request_content)
+            or return $app->error(500, MT::Atom::Entry->errstr);
+    }
+    $atom;
+}
+
+# $target_zone is expected to be a number of hours from GMT
+sub iso2ts {
+    my $app = shift;
+    my($ts, $target_zone) = @_;
+    return unless $ts =~ /^(\d{4})(?:-?(\d{2})(?:-?(\d\d?)(?:T(\d{2}):(\d{2}):(\d{2})(?:\.\d+)?(?:Z|([+-]\d{2}:\d{2}))?)?)?)?/;
+    my($y, $mo, $d, $h, $m, $s, $zone) =
+        ($1, $2 || 1, $3 || 1, $4 || 0, $5 || 0, $6 || 0, $7);
+    if ($zone) {
+        my ($zh, $zm) = $zone =~ /([+-]\d\d):(\d\d)/;
+        use Time::Local qw( timegm );
+        my $ts = timegm( $s, $m, $h, $d, $mo - 1, $y - 1900 );
+        if ($zone ne 'Z') {
+            require MT::DateTime;
+            my $tz_secs = MT::DateTime->tz_offset_as_seconds($zone);
+            $ts -= $tz_secs;
+        }
+        if ($target_zone) {
+            my $tz_secs = (3600 * int($target_zone) + 
+                           60 * abs($target_zone - int($target_zone)));
+            $ts += $tz_secs;
+        }
+        ($s, $m, $h, $d, $mo, $y) = gmtime( $ts );
+        $y += 1900; $mo++;
+    }
+    sprintf("%04d%02d%02d%02d%02d%02d", $y, $mo, $d, $h, $m, $s);
+}
+
+sub iso2epoch {
+    my $app = shift;
+    my($ts) = @_;
+    return unless $ts =~ /^(\d{4})(?:-?(\d{2})(?:-?(\d\d?)(?:T(\d{2}):(\d{2}):(\d{2})(?:\.\d+)?(?:Z|([+-]\d{2}:\d{2}))?)?)?)?/;
+    my($y, $mo, $d, $h, $m, $s, $zone) =
+        ($1, $2 || 1, $3 || 1, $4 || 0, $5 || 0, $6 || 0, $7);
+
+    use Time::Local;
+    my $dt = timegm($s, $m, $h, $d, $mo-1, $y);
+    if ($zone && $zone ne 'Z') {
+        require MT::DateTime;
+        my $tz_secs = MT::DateTime->tz_offset_as_seconds($zone);
+        $dt -= $tz_secs;
+    }
+    $dt;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+MT::AtomServer
+
+=head1 SYNOPSIS
+
+An Atom Publishing API interface for communicating with Movable Type.
+
+=head1 METHODS
+
+=head2 $app->xml_body()
+
+Takes the content posted to the server and parses it into an XML document.
+Uses either XML::LibXML or XML::XPath depending on which is available.
+
+=head2 $app->iso2epoch($iso_ts)
+
+Converts C<$iso_ts> in the format of an ISO timestamp into a unix timestamp
+(seconds since the epoch).
+
+=head2 $app->init
+
+Initializes the application.
+
+=head2 $app->get_auth_info
+
+Processes the request for WSSE authentication and returns a hash containing:
+
+=over 4
+
+=item * Username
+
+=item * PasswordDigest
+
+=item * Nonce
+
+=item * Created
+
+=back
+
+=head2 $app->handle_request
+
+The implementation of this in I<MT::AtomServer::Weblog> passes the request
+to the proper method.
+
+=head2 $app->handle
+
+Wrapper method that determines the proper AtomServer package to pass the
+request to.
+
+=head2 $app->iso2ts($iso_ts, $target_zone)
+
+Converts C<$iso_ts> in the format of an ISO timestamp into a MT-compatible
+timestamp (YYYYMMDDHHMMSS) for the specified timezone C<$target_zone>.
+
+=head2 $app->atom_body
+
+Processes the request as Atom content and returns an XML::Atom object.
+
+=head2 $app->error($code, $message)
+
+Sends the HTTP headers necessary to relay an error.
+
+=head2 $app->authenticate()
+
+Checks the WSSE authentication with the local MT user database and
+confirms the user is authorized to access the resources required by
+the request.
+
+=head2 $app->show_error($message)
+
+Returns an XML wrapper for the error response.
+
+=head2 $app->auth_failure($code, $message)
+
+Handles the response in the event of an authentication failure.
+
+=head1 CALLBACKS
+
+=over 4
+
+=item api_pre_save.entry
+
+    callback($eh, $app, $entry, $original_entry)
+
+Called before saving a new or existing entry. If saving a new entry, the
+$original_entry will have an unassigned 'id'. This callback is executed
+as a filter, so your handler must return 1 to allow the entry to be saved.
+
+=item api_post_save.entry
+
+    callback($eh, $app, $entry, $original_entry)
+
+Called after saving a new or existing entry. If saving a new entry, the
+$original_entry will have an unassigned 'id'.
+
+=back
+
+=cut
---
+++ plugins/AtomServer/lib/MT/App/Atompub/Legacy.pm
@@ -0,0 +1,216 @@
+# Movable Type (r) Open Source (C) 2001-2008 Six Apart, Ltd.
+# This program is distributed under the terms of the
+# GNU General Public License, version 2.
+#
+# $Id: AtomServer.pm 1174 2008-01-08 21:02:50Z bchoate $
+
+package MT::AtomServer::Legacy;
+use strict;
+
+use base qw( MT::AtomServer::Weblog );
+
+use MT::I18N qw( encode_text );
+use XML::Atom;  # for LIBXML
+use XML::Atom::Feed;
+use base qw( MT::AtomServer );
+use MT::Blog;
+use MT::Permission;
+
+use constant NS_CATEGORY => 'http://sixapart.com/atom/category#';
+use constant NS_DC => MT::AtomServer::Weblog->NS_DC();
+
+sub script { $_[0]->{cfg}->AtomScript . '/weblog' }
+
+sub atom_content_type   { 'application/xml' }
+sub atom_x_content_type { 'application/x.atom+xml' }
+
+sub edit_link_rel { 'service.edit' }
+sub get_posts_order_field { 'authored_on' }
+
+sub new_feed {
+    my $app = shift;
+    XML::Atom::Feed->new();
+}
+
+sub new_with_entry {
+    my $app = shift;
+    my ($entry) = @_;
+    MT::Atom::Entry->new_with_entry($entry);
+}
+
+sub apply_basename {}
+
+sub get_weblogs {
+    my $app = shift;
+    my $user = $app->{user};
+    my $iter = $user->is_superuser
+        ? MT::Blog->load_iter()
+        : MT::Permission->load_iter({ author_id => $user->id });
+    my $feed = $app->new_feed();
+    my $base = $app->base . $app->uri;
+    while (my $thing = $iter->()) {
+        if ($thing->isa('MT::Permission')) {
+            next unless $thing->can_create_post;
+        }
+        my $blog = $thing->isa('MT::Blog') ? $thing
+            : MT::Blog->load($thing->blog_id);
+        my $uri = $base . '/blog_id=' . $blog->id;
+        my $blogname = encode_text($blog->name . ' #' . $blog->id, undef, 'utf-8');
+        $feed->add_link({ rel => 'service.post', title => $blogname,
+                          href => $uri, type => 'application/x.atom+xml' });
+        $feed->add_link({ rel => 'service.feed', title => $blogname,
+                          href => $uri, type => 'application/x.atom+xml' });
+        $feed->add_link({ rel => 'service.upload', title => $blogname,
+                          href => $uri . '/svc=upload',
+                          type => 'application/x.atom+xml' });
+        $feed->add_link({ rel => 'service.categories', title => $blogname,
+                          href => $uri . '/svc=categories',
+                          type => 'application/x.atom+xml' });
+        $feed->add_link({ rel => 'alternate', title => $blogname,
+                          href => $blog->site_url,
+                          type => 'text/html' });
+    }
+    $app->response_code(200);
+    $app->response_content_type('application/x.atom+xml');
+    $feed->as_xml;
+}
+
+sub get_categories {
+    my $app = shift;
+    my $blog = $app->{blog};
+    my $iter = MT::Category->load_iter({ blog_id => $blog->id });
+    my $doc;
+    if (LIBXML) {
+        $doc = XML::LibXML::Document->createDocument('1.0', 'utf-8');
+        my $root = $doc->createElementNS(NS_CATEGORY, 'categories');
+        $doc->setDocumentElement($root);
+    } else {
+        $doc = XML::XPath::Node::Element->new('categories');
+        my $ns = XML::XPath::Node::Namespace->new('#default' => NS_CATEGORY);
+        $doc->appendNamespace($ns);
+    }
+    while (my $cat = $iter->()) {
+        my $catlabel = encode_text($cat->label, undef, 'utf-8');
+        if (LIBXML) {
+            my $elem = $doc->createElementNS(NS_DC, 'subject');
+            $doc->getDocumentElement->appendChild($elem);
+            $elem->appendChild(XML::LibXML::Text->new($catlabel));
+        } else {
+            my $elem = XML::XPath::Node::Element->new('subject');
+            my $ns = XML::XPath::Node::Namespace->new('#default' => NS_DC);
+            $elem->appendNamespace($ns);
+            $doc->appendChild($elem);
+            $elem->appendChild(XML::XPath::Node::Text->new($catlabel));
+        }
+    }
+    $app->response_code(200);
+    $app->response_content_type('application/x.atom+xml');
+    if (LIBXML) {
+        $doc->toString(1);
+    } else {
+        return '<?xml version="1.0" encoding="utf-8"?>' . "\n" . $doc->toString;
+    }
+}
+
+1;
+__END__
+
+=head1 NAME
+
+MT::AtomServer
+
+=head1 SYNOPSIS
+
+An Atom Publishing API interface for communicating with Movable Type.
+
+=head1 METHODS
+
+=head2 $app->xml_body()
+
+Takes the content posted to the server and parses it into an XML document.
+Uses either XML::LibXML or XML::XPath depending on which is available.
+
+=head2 $app->iso2epoch($iso_ts)
+
+Converts C<$iso_ts> in the format of an ISO timestamp into a unix timestamp
+(seconds since the epoch).
+
+=head2 $app->init
+
+Initializes the application.
+
+=head2 $app->get_auth_info
+
+Processes the request for WSSE authentication and returns a hash containing:
+
+=over 4
+
+=item * Username
+
+=item * PasswordDigest
+
+=item * Nonce
+
+=item * Created
+
+=back
+
+=head2 $app->handle_request
+
+The implementation of this in I<MT::AtomServer::Weblog> passes the request
+to the proper method.
+
+=head2 $app->handle
+
+Wrapper method that determines the proper AtomServer package to pass the
+request to.
+
+=head2 $app->iso2ts($iso_ts, $target_zone)
+
+Converts C<$iso_ts> in the format of an ISO timestamp into a MT-compatible
+timestamp (YYYYMMDDHHMMSS) for the specified timezone C<$target_zone>.
+
+=head2 $app->atom_body
+
+Processes the request as Atom content and returns an XML::Atom object.
+
+=head2 $app->error($code, $message)
+
+Sends the HTTP headers necessary to relay an error.
+
+=head2 $app->authenticate()
+
+Checks the WSSE authentication with the local MT user database and
+confirms the user is authorized to access the resources required by
+the request.
+
+=head2 $app->show_error($message)
+
+Returns an XML wrapper for the error response.
+
+=head2 $app->auth_failure($code, $message)
+
+Handles the response in the event of an authentication failure.
+
+=head1 CALLBACKS
+
+=over 4
+
+=item api_pre_save.entry
+
+    callback($eh, $app, $entry, $original_entry)
+
+Called before saving a new or existing entry. If saving a new entry, the
+$original_entry will have an unassigned 'id'. This callback is executed
+as a filter, so your handler must return 1 to allow the entry to be saved.
+
+=item api_post_save.entry
+
+    callback($eh, $app, $entry, $original_entry)
+
+Called after saving a new or existing entry. If saving a new entry, the
+$original_entry will have an unassigned 'id'.
+
+=back
+
+=cut
